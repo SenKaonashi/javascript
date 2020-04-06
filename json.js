cac37e771f7db186335b496ad64657867cfc389d
mSato <50111304+SenKaonashi@users.noreply.github.com>
2020-04-06 22:55:08 +0900

    Create json.js
    json test

---
+++ json.js
@@ -0,0 +1,14 @@
+// https://developer.mozilla.org/ja/docs/Learn/JavaScript/Objects/JSON
+// dev tool with google
+var jsontest=  {
+    "name": "Molecule Man",
+    "age": 39,
+    "secretIdentity": "Dan Jukes",
+    "powers": [
+      "Radiation resistance",
+      "Turning tiny",
+      "Radiation blast"
+    ]
+  }
+  console.log(jsontest);
+  JSON.stringify(jsontest);
