1795886d6d0517f8f463e8213a70055ae8fa847b
mSato <50111304+SenKaonashi@users.noreply.github.com>
2020-04-07 22:37:58 +0900

    js testcode

--- json.js
+++ json.js
@@ -12,3 +12,21 @@ var jsontest=  {
   }
   console.log(jsontest);
   JSON.stringify(jsontest);
+
+// test url
+// https://developer.mozilla.org/ja/docs/Web/API/Fetch_API/Using_Fetch
+var content = "Hello World";
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "text/plain");
+myHeaders.append("Content-Length", content.length.toString());
+myHeaders.append("X-Custom-Header", "ProcessThisImmediately");
+
+
+console.log(myHeaders.has("Content-Type")); // true
+console.log(myHeaders.has("Set-Cookie")); // false
+myHeaders.set("Content-Type", "text/html");
+myHeaders.append("X-Custom-Header", "AnotherValue");
+console.log(myHeaders.get("Content-Length")); // 11
+console.log(myHeaders.get("X-Custom-Header")); // ["ProcessThisImmediately", "AnotherValue"]
+myHeaders.delete("X-Custom-Header");
+console.log(myHeaders.get("X-Custom-Header")); // [ ]
